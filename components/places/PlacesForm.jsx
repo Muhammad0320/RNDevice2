d0c8415a4d480b1d51d640d5df71d78373ae5069
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-03 13:21:09 +0100

    chore: add place form comp

---
+++ components/places/PlacesForm.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+function PlacesForm() {
+  return (
+    <View>
+      <Text> The place Form comp </Text>
+    </View>
+  );
+}
+
+export default PlacesForm;
