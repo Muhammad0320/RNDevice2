6719c4b62c103cce499c28f553ad445a058c6bda
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-03 14:47:31 +0100

    chore: add screens to app.js screen

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+
+import { NavigationContainer } from "@react-navigation/native";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AllPlaces from "./screens/AllPlaces";
+import AddPlace from "./screens/AddPlace";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
+
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="AllPlaces" component={AllPlaces} />
+
+          <Stack.Screen name="AddPlaces" component={AddPlace} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
