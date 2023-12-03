4f19e9e981c074cda8612db37cae8b81bfd457d2
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-03 12:22:06 +0100

    add place

---
+++ model/Place.jsx
@@ -0,0 +1,10 @@
+class Place {
+  constructor(title, location, address, imageUri) {
+    this.title = title;
+    this.location = location;
+    this.address = address;
+    this.imageUri = imageUri;
+  }
+}
+
+export default Place;
