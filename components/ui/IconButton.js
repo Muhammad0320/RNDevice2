0062f1c60dd40f9aad18ad460bda7dc37a6ccd11
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-03 15:34:20 +0100

    chore: complete icon button comp

---
+++ components/ui/IconButton.js
@@ -0,0 +1,28 @@
+import { Pressable, StyleSheet } from "react-native";
+
+import { Ionicons } from "@expo/vector-icons";
+
+function IconButton({ icon, color, size, onPress }) {
+  return (
+    <Pressable
+      onPress={onPress}
+      style={({ pressed }) => [styles.button, pressed && styles.pressed]}
+    >
+      <Ionicons name={icon} size={size} color={color} />
+    </Pressable>
+  );
+}
+
+export default IconButton;
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 8,
+    justifyContent: "center",
+    alignContent: "center",
+  },
+
+  pressed: {
+    opacity: 0.75,
+  },
+});
