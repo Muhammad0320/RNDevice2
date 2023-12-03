0a5e2a1749bacd54e5334a0a08e47b627866f6d6
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-03 15:38:17 +0100

    chore: update app.js

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import AllPlaces from "./screens/AllPlaces";
 import AddPlace from "./screens/AddPlace";
+import IconButton from "./components/ui/IconButton";
 
 const Stack = createNativeStackNavigator();
 
@@ -15,7 +16,21 @@ export default function App() {
 
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="AllPlaces" component={AllPlaces} />
+          <Stack.Screen
+            name="AllPlaces"
+            component={AllPlaces}
+            options={({ navigation }) => ({
+              title: "Your favourite places",
+              headerRight: ({ tintColor }) => (
+                <IconButton
+                  color={tintColor}
+                  icon={"add"}
+                  size={24}
+                  onPress={() => navigation.navigate("AddPlaces")}
+                />
+              ),
+            })}
+          />
 
           <Stack.Screen name="AddPlaces" component={AddPlace} />
         </Stack.Navigator>
--- components/ui/IconButton.js
+++ components/ui/IconButton.js
@@ -1,6 +1,5 @@
-import { Pressable, StyleSheet } from "react-native";
-
 import { Ionicons } from "@expo/vector-icons";
+import { Pressable, StyleSheet } from "react-native";
 
 function IconButton({ icon, color, size, onPress }) {
   return (
