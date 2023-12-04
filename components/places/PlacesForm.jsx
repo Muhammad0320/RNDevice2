c6ea32583df44ed54fd46015fbfaeaa42a8d8978
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-04 17:27:15 +0100

    chore: add data to placeForm

--- components/places/PlacesForm.jsx
+++ components/places/PlacesForm.jsx
@@ -1,11 +1,53 @@
-import { Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+import { Colors } from "../../util/colors";
+import { useState } from "react";
 
 function PlacesForm() {
+  const [label, setLabel] = useState("");
+
+  const handleChangeText = (enteredText) => {
+    setLabel(enteredText);
+  };
+
   return (
-    <View>
-      <Text> The place Form comp </Text>
-    </View>
+    <ScrollView style={styles.form}>
+      <View style={styles.container}>
+        <Text style={styles.label}>{label}</Text>
+
+        <TextInput
+          style={styles.input}
+          onChangeText={handleChangeText}
+          value={label}
+        />
+      </View>
+    </ScrollView>
   );
 }
 
 export default PlacesForm;
+
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+
+  label: {
+    fontWeight: "bold",
+    color: Colors.primary500,
+  },
+
+  container: {
+    rowGap: 8,
+  },
+
+  input: {
+    backgroundColor: Colors.primary100,
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    borderBottomWidth: 2,
+    borderBottomColor: Colors.primary700,
+    fontSize: 18,
+  },
+});
