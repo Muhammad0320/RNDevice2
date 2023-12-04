5a1525a1ebdf5c192648ffa90348452a26765d36
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-04 17:52:22 +0100

    chore: add image picker

---
+++ components/places/ImagePicker.jsx
@@ -0,0 +1,12 @@
+import { Button, View } from "react-native";
+
+function ImagePicker() {
+  return (
+    <View>
+      <View></View>
+      <Button title="Take image" />
+    </View>
+  );
+}
+
+export default ImagePicker;
