d9c4d35f6bb8a462b3271a8c4dd7105beb860195
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-04 17:55:40 +0100

    chore: configured image picker

--- components/places/ImagePicker.jsx
+++ components/places/ImagePicker.jsx
@@ -1,10 +1,21 @@
+import { launchCameraAsync } from "expo-image-picker";
 import { Button, View } from "react-native";
 
 function ImagePicker() {
+  const handleTakeImage = async () => {
+    const image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.6,
+    });
+
+    console.log(image);
+  };
+
   return (
     <View>
       <View></View>
-      <Button title="Take image" />
+      <Button title="Take image" onPress={handleTakeImage} />
     </View>
   );
 }
