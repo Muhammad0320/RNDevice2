d9726ad5697d838b81a18d3aefb02e1679c2151e
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-05 15:55:39 +0100

    chore: add location func

---
+++ util/location.js
@@ -0,0 +1,8 @@
+GOOGLE_API_KEY = "AIzaSyCFe5gDVVJtzXrpD8JxPJtK6m9_s9rcWPk";
+
+export function getLocationPreview(lat, lng) {
+  const location = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=13&size=300x200&maptype=roadmap
+      &markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
+
+  return location;
+}
