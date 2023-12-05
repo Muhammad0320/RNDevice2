e5465cc344dc34db96a7bf7b3b3c37c5f7ccd809
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-05 16:11:56 +0100

    chore: update location

--- components/places/LocationPicker.jsx
+++ components/places/LocationPicker.jsx
@@ -1,16 +1,74 @@
-import { View } from "react-native";
+import { Alert, Image, StyleSheet, View } from "react-native";
 import OutlinedButton from "../ui/OutlinedButton";
+import { useState } from "react";
+import {
+  useForegroundPermissions,
+  PermissionStatus,
+  getCurrentPositionAsync,
+} from "expo-location";
+import { getLocationPreview } from "../../util/location";
 
 function LocationPicker() {
-  const handleCurrentLocation = () => {};
+  const [locationPosition, setLocationPosition] = useState({});
+
+  const [locationPermissionInformation, requestPermissions] =
+    useForegroundPermissions();
+
+  const verifyLocation = async () => {
+    if (
+      locationPermissionInformation.status === PermissionStatus.UNDETERMINED
+    ) {
+      const permissions = await requestPermissions();
+
+      return permissions.granted;
+    }
+
+    if (locationPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient permission",
+        "This app needs permission to your location to work properly"
+      );
+
+      return false;
+    }
+
+    return true;
+  };
+
+  const handleCurrentLocation = async () => {
+    const hasPermission = await verifyLocation();
+
+    if (!hasPermission) {
+      return;
+    }
+
+    const location = await getCurrentPositionAsync();
+
+    setLocationPosition({
+      lat: location.coords.latitude,
+      lng: location.coords.longitude,
+    });
+  };
 
   const handlePickLocation = () => {};
 
+  let content = <Text> There is no location yet - start by picking one </Text>;
+
+  if (locationPosition) {
+    content = (
+      <Image
+        source={{
+          uri: getLocationPreview(locationPosition.lat, locationPosition.lng),
+        }}
+      />
+    );
+  }
+
   return (
     <View>
-      <View> </View>
+      <View style={styles.container}>{content}</View>
 
-      <View>
+      <View style={styles.buttonContainer}>
         <OutlinedButton icon="location" onPress={handleCurrentLocation}>
           current location
         </OutlinedButton>
@@ -23,3 +81,23 @@ function LocationPicker() {
 }
 
 export default LocationPicker;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.primary100,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: 200,
+    borderRadius: 4,
+
+    marginVertical: 10,
+    overflow: "hidden",
+  },
+
+  buttonContainer: {
+    justifyContent: "space-around",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+});
