50dc0c9fea35da884f7cfa6184ebbb6e506945c3
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-05 06:42:58 +0100

    chore: request camera permission

--- components/places/ImagePicker.jsx
+++ components/places/ImagePicker.jsx
@@ -1,8 +1,39 @@
-import { launchCameraAsync } from "expo-image-picker";
-import { Button, View } from "react-native";
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from "expo-image-picker";
+import { Alert, Button, View } from "react-native";
 
 function ImagePicker() {
+  const [cameraPermissionInfo, requestPermission] = useCameraPermissions();
+
+  const verifyPermission = async () => {
+    if (cameraPermissionInfo.status === PermissionStatus.UNDETERMINED) {
+      const requestPermit = await requestPermission();
+
+      return requestPermit.granted;
+    }
+
+    if (cameraPermissionInfo.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient permission",
+        "This app need camera permisstion to function properly"
+      );
+
+      return false;
+    }
+
+    return true;
+  };
+
   const handleTakeImage = async () => {
+    const hasPermission = await verifyPermission();
+
+    if (!hasPermission) {
+      return false;
+    }
+
     const image = await launchCameraAsync({
       allowsEditing: true,
       aspect: [16, 9],
