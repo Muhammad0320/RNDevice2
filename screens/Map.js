9e17cb8960171093833923651fd69d7961b13bdf
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-06 19:10:21 +0100

    chore: add maps marker when map is pressed

--- screens/Map.js
+++ screens/Map.js
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import { StyleSheet } from "react-native";
 import { Marker } from "react-native-maps";
 
 import MapView from "react-native-maps";
 
 function Map() {
+  const [selectedLocation, setSelectedLocation] = useState({});
+
   const initialPosition = {
     latitude: 8.4911976,
     longitide: 4.6700633,
@@ -11,11 +14,23 @@ function Map() {
     longitideDelta: 0.0421,
   };
 
+  const handlePress = (event) => {
+    const { latitude, longitide } = event.nativeEvent.coordinate;
+
+    setSelectedLocation({ latitude, longitide });
+  };
+
   // 8.4911976,4.6700633
 
   return (
-    <MapView style={styles.root} initialRegion={initialPosition}>
-      {" "}
+    <MapView
+      style={styles.root}
+      onPress={handlePress}
+      initialRegion={initialPosition}
+    >
+      {selectedLocation && (
+        <Marker title="Picked location" coordinate={{ ...selectedLocation }} />
+      )}
     </MapView>
   );
 }
