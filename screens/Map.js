a659877f1403d1c69035b66b8835db29e27ec63e
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-06 18:35:54 +0100

    chore: add map.js to configuration

--- screens/Map.js
+++ screens/Map.js
@@ -1,11 +1,29 @@
-import { Text } from "react-native";
+import { StyleSheet } from "react-native";
+import { Marker } from "react-native-maps";
+
+import MapView from "react-native-maps";
 
 function Map() {
+  const initialPosition = {
+    latitude: 8.4911976,
+    longitide: 4.6700633,
+    latitudeDelta: 0.00224,
+    longitideDelta: 0.0421,
+  };
+
+  // 8.4911976,4.6700633
+
   return (
-    <>
-      <Text> The map screen </Text>
-    </>
+    <MapView style={styles.root} initialRegion={initialPosition}>
+      {" "}
+    </MapView>
   );
 }
 
 export default Map;
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+});
