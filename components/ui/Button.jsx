2eae7c406e678c02cfe8701d43c03949c713c1c6
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-27 05:10:39 +0100

    chore: styled the buttons

--- components/ui/Button.jsx
+++ components/ui/Button.jsx
@@ -1,7 +1,35 @@
-import { Pressable } from "react-native";
+import { Pressable, StyleSheet, Text } from "react-native";
+import { Colors } from "../../util/colors";
 
-function Button() {
-  return <Pressable></Pressable>;
+function Button({ children, onPress }) {
+  return (
+    <Pressable onPress={onPress}>
+      <Text>{children}</Text>
+    </Pressable>
+  );
 }
 
 export default Button;
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 5,
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+    shadowOffset: { height: 0.5, width: 2 },
+    backgroundColor: Colors.primary800,
+    borderRadius: 8,
+  },
+  text: {
+    fontSize: 18,
+    color: Colors.primary50,
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
