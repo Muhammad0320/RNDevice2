41e7211b6d32b0b054085c0d19294fd38410106c
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-28 07:27:06 +0100

    chore: update data

--- screens/AllPlaces.js
+++ screens/AllPlaces.js
@@ -3,13 +3,13 @@ import PlacesList from "../components/places/placesList";
 import { useIsFocused } from "@react-navigation/native";
 
 function AllPlaces({ route }) {
-  const [enteredData, setEnteredData] = useState({});
+  const [enteredData, setEnteredData] = useState([]);
 
   const isFocus = useIsFocused();
 
   useEffect(() => {
     if ((isFocus, route.params.data)) {
-      setEnteredData(route.params.data);
+      setEnteredData((data) => [...data, route.params.data]);
     }
   }, [isFocus, route.params]);
 
