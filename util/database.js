4bd59eff54f6089831f1d49a6b7be345d69188f3
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-28 09:05:48 +0100

    chore: wrapped db transaction in promise

--- util/database.js
+++ util/database.js
@@ -3,18 +3,31 @@ import * as SQLite from "expo-sqlite";
 const database = SQLite.openDatabase("place.db");
 
 export const init = () => {
-  database.transaction((tx) => {
-    tx.executeSql(` 
+  const promise = new Promise((resolve, reject) => {
+    database.transaction((tx) => {
+      tx.executeSql(
+        ` 
+        
+        
+                    CREATE TABLE IF NOT EXISTS places (
+                        id INTEGER PRIMARY KEY NOT NULL,
+                        title TEXT NOT NULL,
+                        lat REAL NOT NULL,
+                        lng REAL NOT NULL,
+                        imageUri TEXT NOT NULL,
+                        address TEXT NOT NULL
     
-    
-                CREATE TABLE IF NOT EXISTS places (
-                    id INTEGER PRIMARY KEY NOT NULL,
-                    title TEXT NOT NULL,
-                    lat REAL NOT NULL,
-                    lng REAL NOT NULL,
-                    imageUri TEXT NOT NULL,
-                    address TEXT NOT NULL
-
-        ) `);
+            ) `,
+        [],
+        (data) => {
+          resolve(data);
+        },
+        (error) => {
+          reject(error);
+        }
+      );
+    });
   });
+
+  promise();
 };
