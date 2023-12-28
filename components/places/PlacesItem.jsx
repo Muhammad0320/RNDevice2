65869a9085fb56f3e5f962cc12a45c6c3eecedbe
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-28 07:21:29 +0100

    chore: add place item styles

--- components/places/PlacesItem.jsx
+++ components/places/PlacesItem.jsx
@@ -1,4 +1,5 @@
 import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import { Colors } from "../../util/colors";
 
 function PlacesItem({ imageUri, title, address }) {
   return (
@@ -14,4 +15,41 @@ function PlacesItem({ imageUri, title, address }) {
 
 export default PlacesItem;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.primary500,
+    elevation: 4,
+    borderRadius: 6,
+
+    flexDirection: "row",
+    alignItems: "flex-start",
+    overflow: "hidden",
+  },
+
+  textContainer: {
+    flex: 2,
+    rowGap: 4,
+    padding: 6,
+  },
+
+  title: {
+    fontsize: 18,
+    fontWeight: "bold",
+    color: Colors.primary800,
+  },
+
+  address: {
+    fontsize: 13,
+    color: Colors.primary800,
+  },
+
+  pressed: {
+    opacity: 0.85,
+  },
+
+  image: {
+    flex: 1,
+    height: 100,
+  },
+});
