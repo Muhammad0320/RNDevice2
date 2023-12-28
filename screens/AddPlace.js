200cd79cefa6107fadc9804d1d12f72444a19b79
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-28 06:43:42 +0100

    chore: update the add place comp

--- screens/AddPlace.js
+++ screens/AddPlace.js
@@ -1,7 +1,11 @@
 import PlacesForm from "../components/places/PlacesForm";
 
-function AddPlace() {
-  return <PlacesForm />;
+function AddPlace({ navigation }) {
+  const handleSaveData = (data) => {
+    navigation.navigate("AllPlaces", { data });
+  };
+
+  return <PlacesForm onSaveData={handleSaveData} />;
 }
 
 export default AddPlace;
