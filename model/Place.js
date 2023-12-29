b028e589fc0e2d563b1b7e6a9d556d555dffcde2
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-29 21:05:10 +0100

    chore: update place constructir

--- model/Place.js
+++ model/Place.js
@@ -1,10 +1,10 @@
 class Place {
-  constructor(title, location, imageUri) {
+  constructor(title, location, imageUri, id) {
     this.title = title;
     this.location = { lat: location.lat, lng: location.lng };
     this.address = location.address;
     this.imageUri = imageUri;
-    id: Date.now() + Math.random();
+    this.id = id;
   }
 }
 
