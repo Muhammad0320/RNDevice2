c8c9048e7c398a9249cb1b45619f56a3a4a939b4
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-30 09:51:11 +0100

    chore: add styles to place details

--- screens/PlaceDetails.js
+++ screens/PlaceDetails.js
@@ -1,9 +1,49 @@
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import { Colors } from "../util/colors";
+import OutlinedButton from "../components/ui/OutlinedButton";
+
 function PlaceDetails() {
+  const onViewMap = () => {};
+
   return (
-    <>
-      <Text> The place details </Text>
-    </>
+    <ScrollView>
+      <Image style={styles.image} />
+
+      <View>
+        <View>
+          <Text>Adress</Text>
+        </View>
+
+        <OutlinedButton icon="map" onPress={onViewMap}>
+          {" "}
+          View on Map{" "}
+        </OutlinedButton>
+      </View>
+    </ScrollView>
   );
 }
 
 export default PlaceDetails;
+
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: "35%",
+    minHeight: 300,
+  },
+
+  locationContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  addressContainer: {
+    textAlign: "center",
+  },
+
+  text: {
+    color: Colors.primary500,
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+});
